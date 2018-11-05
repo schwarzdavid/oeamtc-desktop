4b641fa6f5fec3f52a0de982db8bfeaa8fd4befd
David Schwarz <schwarz.david10@gmail.com>
2018-11-05 01:42:46 +0100

    Default state updated

--- public/src/index.ts
+++ public/src/index.ts
@@ -1,17 +1,4 @@
 import Oeamtc from 'core/Core.vue';
-import Vue from 'vue';
 import './routes';
-import './config';
-import store from './core/store';
 
-Promise.resolve()
-    .then(() => {
-        const localUser = JSON.parse(localStorage.getItem('user'));
-        return store.dispatch('user/login', localUser);
-    })
-    .catch(err => {
-        console.error('Cannot load user');
-    })
-    .then(() => {
-        new Oeamtc().$mount('#app');
-    });
+new Oeamtc().$mount('#app');
