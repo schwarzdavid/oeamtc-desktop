c1e58bd2a311de503303636eb7943bc7dea2b9d4
David Schwarz <schwarz.david10@gmail.com>
2018-11-08 22:32:55 +0100

    Router logic updated

--- public/src/routes.ts
+++ public/src/routes.ts
@@ -4,6 +4,8 @@ import ArrivingComponent from './core/components/mission/Arriving.vue';
 import AtWorkComponent from './components/mission/AtWork.vue'
 import MovingOnComponent from './core/components/mission/MovingOn.vue';
 import {config} from "./core/plugins/config/lib";
+import store from "./core/store";
+import {UserState} from "./core/store/user/types";
 
 const routes = config.get('routes');
 
@@ -16,17 +18,26 @@ router.addRoutes([
             {
                 path: 'arriving',
                 name: routes.arriving,
-                component: ArrivingComponent
+                component: ArrivingComponent,
+                meta: {
+                    requireState: UserState.ARRIVING
+                }
             },
             {
                 path: 'at-work',
                 name: routes.atWork,
-                component: AtWorkComponent
+                component: AtWorkComponent,
+                meta: {
+                    requireState: UserState.AT_WORK
+                }
             },
             {
                 path: 'moving-on',
                 name: routes.movingOn,
-                component: MovingOnComponent
+                component: MovingOnComponent,
+                meta: {
+                    requireState: UserState.MOVING_ON
+                }
             }
         ]
     }
